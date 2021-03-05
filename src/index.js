3c1b6066cc602fb9d38fe46f9d5d50f5423681e1
gloryyang <gyang2@uw.edu>
2021-03-04 16:14:53 -0800

    Change activities.csv to .json, load data into App component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'; 
 import './index.css';
 import App from './App';
-import activities from './activities.csv';
+import activities from './activities.json';
 import * as d3 from 'd3';
-//import reportWebVitals from './reportWebVitals';
-
-
-let activitiesArr = [];
-
-d3.csv(activities).then(function(activities) {
-  activitiesArr.push(activities);
-  });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App activities={activitiesArr}/>
+    <App activities={activities}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
