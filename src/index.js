3e6ac97e09576358e5669b7c6dd132b8bd615b94
gloryyang <gyang2@uw.edu>
2021-03-06 20:34:48 -0800

    Set up and publish production app to Firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import './index.css';
 import App from './App';
 import activities from './activities.json';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApJ0kYV22GpwhLl4m1ltQpJxs_4jNDrjM",
+  authDomain: "quarantivities-2.firebaseapp.com",
+  projectId: "quarantivities-2",
+  storageBucket: "quarantivities-2.appspot.com",
+  messagingSenderId: "332862832622",
+  appId: "1:332862832622:web:965f4b5521422d7dfcd8ff",
+  measurementId: "G-YP23439M29"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   (<React.StrictMode>
